21d83ff06527ab2bce9976c7dbf56bb37b9b3359
Allen Christ A <allenchrist430@gmail.com>
2026-01-27 15:29:35 +0530

    Add output example for lab1b.c

--- lab1b/lab1b.c
+++ lab1b/lab1b.c
@@ -24,3 +24,23 @@ int main()
     closedir(d);
     return 0;
 }
+
+output:-
+    ~/os lab$ ls
+2026-01-13-file-1.term  2026-01-13-file-2.term  2026-01-27-file-1.term  lab1a  lab1a.c  lab1b.c  lab2  lab2.c
+~/os lab$ gcc lab1b.c -o lab1b
+~/os lab$ ./lab1b
+Directory contents:
+.
+..
+lab2.c
+2026-01-27-file-1.term
+lab2
+2026-01-13-file-1.term
+lab1a
+lab1b.c
+lab1b
+lab1a.c
+2026-01-13-file-2.term
+~/os lab$ 
+
